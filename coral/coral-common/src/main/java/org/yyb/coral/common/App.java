0aafc62b40be8a1548326e9353621a8f31012688
yinyanbing <=yybg12345678@163.com>
2017-07-12 15:42:16 +0800

    first

---
+++ coral/coral-common/src/main/java/org/yyb/coral/common/App.java
@@ -0,0 +1,13 @@
+package org.yyb.coral.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
